6063e45e50394acc4557d3ad5c00dc297596ae51
jripperger <rippergerjacob@gmail.com>
2021-03-16 22:33:29 -0400

    added user route

---
+++ server/routes/users-api.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+const users = require('./users');
+
+router.get('/', (req, res) => users.getAll(req, res));
+
+module.exports = router;
