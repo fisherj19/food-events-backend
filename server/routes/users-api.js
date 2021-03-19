cd64a74475cd11561a3a84491d687fb37dcd0552
Jason Fisher <jason@wanax.com>
2021-03-19 08:31:45 -0400

    added support for saving user to database; changed admin test

--- server/routes/users-api.js
+++ server/routes/users-api.js
@@ -3,6 +3,9 @@ const router = express.Router();
 
 const users = require('./users');
 
+router.get('/:id', (req, res) => users.getById(req, res));
 router.get('/', (req, res) => users.getAll(req, res));
+
+router.post('/', (req, res) => users.insert(req, res));
 
 module.exports = router;
