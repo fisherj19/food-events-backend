36d55dcf8a06d4cf420b3f8036163b91d852de76
CameronMills <millsc6@xavier.edu>
2021-04-22 16:24:10 -0400

    Updated other email Notifications

--- server/routes/sendDay.js
+++ server/routes/sendDay.js
@@ -1,13 +1,19 @@
-
+const sender = {
+  sendDay: (req, res) => {
 const SparkPost = require('sparkpost');
 const sparky = new SparkPost('27f270542f7c16beb57198b9b1994fb8cb69e5ed');
+const content = `<html><body><p>There is an upcoming Food Event happening at ${req.body.event_name} at ${req.body.food_start_time} 
+    until ${req.body.food_end_time}, this is happening at ${req.body.event_location} on ${req.body.event_date}. 
+    This event will be ${req.body.event_desc}! : </p></body></html>`
+
+
 
 sparky.transmissions.sendDay({
   options: { sandbox: true },
   content: {
     from: 'sparkpostmail.com',
     subject: 'Food event in 1 Day!',
-    html:'<html><body><p>There is an upcoming Food Event happening at _________ at #:##, in 1 Day!, there will be: </p></body></html>'
+    html: content
   },
   recipients: [ { address: 'millsc6@xavier.edu' } ]
 })
@@ -17,3 +23,7 @@ sparky.transmissions.sendDay({
 .catch(err => {
   console.log('Whoops! Something went wrong');
 });
+}
+};
+
+module.exports = sender;
