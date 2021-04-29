b7763e0e4efafd70740bf49f22d0d4880c116d6e
jripperger <rippergerjacob@gmail.com>
2021-04-28 21:29:47 -0400

    attempt at a bug fix

--- server/routes/events-api.js
+++ server/routes/events-api.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const events = require('./events');
 
 router.get('/:id', (req, res) => events.getById(req, res));
-
+router.get('/', (req, res) => events.getAll(req, res));
+router.post('/admin', (req, res) => events.insert(req, res));
 
 module.exports = router;
